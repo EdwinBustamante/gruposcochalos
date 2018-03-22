ee83affab32971666ff7a7febab923552ea55e33
edwincognos <edwinfcyt@gmail.com>
2018-03-22 19:30:17 -0400

    Edit main arreglado

---
+++ app/src/main/java/com/edwinbustamante/gruposcochalos/CuentaUsuarioArchivos/EditMainCuenta.java
@@ -0,0 +1,32 @@
+package com.edwinbustamante.gruposcochalos.CuentaUsuarioArchivos;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.edwinbustamante.gruposcochalos.R;
+
+public class EditMainCuenta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_main_cuenta);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
